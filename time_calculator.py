7077b5ea59dee8ed5a4b69c277abd85cca067b1a
Abu Yasin Sabik <yasin00sabik@gmail.com>
2022-11-23 19:48:48 -0500

    Fixed output formatting

--- time_calculator.py
+++ time_calculator.py
@@ -35,7 +35,13 @@ def add_time(start, duration, sday = ""):
     
     time_dict = {0: 12, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 12: 12, 13: 1, 14: 2, 15: 3, 16: 4, 17: 5, 18: 6, 19: 7, 20: 8, 21: 9, 22: 10, 23: 11}
 
-    curr_hours_12 = time_dict[curr_hours_24]
+    time_dict = {   0: "12 AM", 1: "1 AM", 2: "2 AM", 3: "3 AM", 4: "4 AM", 5: "5 AM", 
+                    6: "6 AM", 7: "7 AM", 8: "8 AM", 9: "9 AM", 10: "10 AM", 11: "11 AM", 
+                    12: "12 PM", 13: "1 PM", 14: "2 PM", 15: "3 PM", 16: "4 PM", 17: "5 PM", 
+                    18: "6 PM", 19: "7 PM", 20: "8 PM", 21: "9 PM", 22: "10 PM", 23: "11 PM"    }
+
+    curr_hours_12 = time_dict[curr_hours_24].split(" ")[0]
+    am_or_pm = time_dict[curr_hours_24].split(" ")[1]
 
 
 
@@ -54,11 +60,18 @@ def add_time(start, duration, sday = ""):
             break
     
     final_day = days[(curr_index + day_counter) - len(days)]
+    days_later = ""
 
-    if (sday != ""):
-        new_time = str(curr_hours_12) + ":" + str(curr_mins).zfill(2) + " " + final_day + " (" + str(day_counter) + " days later)"
+    if day_counter == 0:
+        days_later = ""
+    elif day_counter == 1:
+        days_later = " (next day)"
     else:
-        new_time = str(curr_hours_12) + ":" + str(curr_mins).zfill(2) + " (" + str(day_counter) + " days later)"
-    
+        days_later = " (" + str(day_counter) + " days later)"
+
+    if sday != "":
+        new_time = str(curr_hours_12) + ":" + str(curr_mins).zfill(2) + " " + am_or_pm + ", " + final_day + days_later
+    else:
+        new_time = str(curr_hours_12) + ":" + str(curr_mins).zfill(2) + " " + am_or_pm + days_later
 
     return new_time
